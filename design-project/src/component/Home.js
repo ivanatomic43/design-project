6388012d141ebb2f8c5f2b13e29deee3c13a5ed5
ivanatomic43 <ivana.tomic96@gmail.com>
2021-05-27 02:52:51 +0200

    update

--- design-project/src/component/Home.js
+++ design-project/src/component/Home.js
@@ -22,18 +22,26 @@ class Home extends Component{
                      
                          <p className="custom-text-1">A new type of salon.</p>
                          
-                         <p className="custom-text-2">A New Type Of Experience</p>
+                         <p className="custom-text-2">A New Type Of Experience.</p>
                          
                          <div className="border-div">
                              <div className="inside-border-div">
                                  <li className="button-link"><a>Contact</a></li>
                              </div>
-                             
-                                
-                            
+                         </div>
+
+                         <div className="flower-three">
+                             <img src="images/cvet 2.png"/>
+                         </div>
+                         
+                         <div className="leaf-one">
+                             <img src="images/leaf2.png"/>
                          </div>
 
                         <div className="bar-div">
+
+                        
+
                             <div className="first-circle">
                                 <img src="images/arrowback.png"/>
                             </div>
@@ -41,6 +49,8 @@ class Home extends Component{
                         </div>
 
                      </div>
+
+                     
 
                      
 
--- design-project/src/component/Sadona.js
+++ design-project/src/component/Sadona.js
@@ -11,14 +11,28 @@ class Sadona extends Component{
             <section>
                 
                 <div className="whitediv">
+                    <div className="container">
+                    
+                    
                      <div className="inside-white-div">
-                     
-                            <text class="sadona-text">Sadona was conceived with the belief that a salon and spa should be a welcome and tranquil experience.</text>
-                            <p className="textcustom">And from the moment you walk through our door, you will see exactly what we mean. Come, let's look around together.</p>
-                            <div className="borderdiv2">
-                                <button className="my-button">Our Story</button>
-                            </div>
                             
+                     <div className="linediv1">
+                                
+                                </div>
+                            <div className="text-div">
+                            <text class="sadona-text">Sadona was conceived with the belief that a salon and spa should be a welcome and tranquil experience.</text>
+                            <p className="sadona-text2">And from the moment you walk through our door, you will see exactly what we mean. Come, let's look around together.</p>
+                            <div className= "border-div">
+                            <div className="inside-border-div">
+                               <li className="button-link"><a href="">Our Story</a></li> 
+                            </div>
+                            </div>
+                            </div>
+                            <div className="linediv2">
+                                
+                                </div>
+                            
+                    </div>
                     </div>
                 </div>
                 
